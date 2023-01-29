38caca532b16b3e1502ca1aeeb08179e8395d1d1
Joseph Frusetta <JosephFrusetta@gmail.com>
2023-01-29 15:41:04 -0800

    Reverting changes

--- funconvo.py
+++ funconvo.py
@@ -8,7 +8,7 @@ restart_sequence = "\nHuman:"
 completion = openai.Completion()
 session_prompt = "The following is a conversation with an AI that helps humans create interesting conversation starters. The AI is intelligent, creative, and clever. The AI will not provide boring nor conventional messages.\n\nHuman: I just met someone with a pink fanny pack. How do I start a conversation with them?\nAI: Did you know fanny packs were one of humankind's oldest accessories? Just make sure to call them bumbags if you ever go to Great Britain.\n\nHuman: What's a good first message to send to Hannah. Her dating profile says she wants someone funny and she loves to snowboard.\nAI: Hi Hannah. Did you know that many resorts outlawed snowboarding when the sport was invented back in the 80's? It was also originally referred to as \"Snurfing\".\n\nHuman:  That's a great first message. How about another?\nAI: If both your parents need a kidney to live and you're the only match in the world - what would you do?\n\nHuman: I just met someone really into cats. What's an interesting way to start a chat?\nAI: Hey there! Did you know that cats purr at the same frequency as an idling diesel engine - around 25-150 hertz? It's a great way to soothe both humans and felines alike."
 
-def ask(question, chat_log= session_prompt): #Interfacing with the OpenAI API
+def ask(question, chat_log=None): #Interfacing with the OpenAI API
     prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
     response = openai.Completion.create(
       prompt=prompt_text,
@@ -19,7 +19,7 @@ def ask(question, chat_log= session_prompt): #Interfacing with the OpenAI API
       frequency_penalty=2,
       presence_penalty=1.7,
       best_of=20,
-      stop=["\n", " Human:", " AI:"]
+      stop=[" Human:", " AI:"]
     )
     conversation = response['choices'][0]['text']
     return str(conversation)
