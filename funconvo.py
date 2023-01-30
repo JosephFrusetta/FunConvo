3fafb694fd2260c87e77bda18a938f3ba464b715
Joseph Frusetta <JosephFrusetta@gmail.com>
2023-01-30 07:07:56 -0800

    Trying to reset chat log

--- funconvo.py
+++ funconvo.py
@@ -9,6 +9,7 @@ completion = openai.Completion()
 session_prompt = "The following is a conversation with an AI that helps humans create interesting conversation starters. The AI is intelligent, creative, and clever. The AI will not provide boring nor conventional messages.\n\nHuman: I just met someone with a pink fanny pack. How do I start a conversation with them?\nAI: Did you know fanny packs were one of humankind's oldest accessories? Just make sure to call them bumbags if you ever go to Great Britain.\n\nHuman: What's a good first message to send to Hannah. Her dating profile says she wants someone funny and she loves to snowboard.\nAI: Hi Hannah. Did you know that many resorts outlawed snowboarding when the sport was invented back in the 80's? It was also originally referred to as \"Snurfing\".\n\nHuman:  That's a great first message. How about another?\nAI: If both your parents need a kidney to live and you're the only match in the world - what would you do?"
 
 def ask(question=None, chat_log=None): #Interfacing with the OpenAI API
+    chat_log = session_prompt
     prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
     response = openai.Completion.create(
       model="text-davinci-003",
@@ -19,12 +20,11 @@ def ask(question=None, chat_log=None): #Interfacing with the OpenAI API
       best_of=20,
       frequency_penalty=2,
       presence_penalty=1.7,
-      stop=["\n", " Human:", " AI:"]
+      stop=[" Human:", " AI:"]
     )
     conversation = response['choices'][0]['text']
     return str(conversation)
 
 def append_interaction_to_chat_log(question=None, answer=None, chat_log=None): #Twilio code that appends a question and response to the chat log.
-    if chat_log is None:
-        chat_log = session_prompt
+    chat_log = session_prompt
     return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
