bc78fc5913f029fe33ffb56bbe520c82d5d1a909
Joseph Frusetta <JosephFrusetta@gmail.com>
2023-03-13 11:45:26 -0700

    Ensuring string for role field

--- funconvo.py
+++ funconvo.py
@@ -6,7 +6,8 @@ from random import choice
 message_history = [{'role': 'system', 'content': "The following is a conversation with an AI that helps humans create interesting conversation starters. The AI is intelligent, creative, and clever. The AI will not provide boring nor conventional messages."}]
 
 def ask(question, role="user", chat_log=""): #Uses OpenAI API to generate a response to the user's input and return the response.
-    message_history.append({"role": role, "content": f"{question}"})   # Add the user input to the chat history
+    role_str = str(role)
+    message_history.append({"role": role_str, "content": f"{question}"})   # Add the user input to the chat history
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=message_history
